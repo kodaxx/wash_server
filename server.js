7fa6a4216d2cd07af0941d454e6588bff66363a6
Spencer Kuzara <kodaxx@gmail.com>
2019-04-18 21:52:27 -0600

    add rpio for control of led lights

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 
 const raspihats = require('raspihats')
 const DQ10rly = raspihats.i2cHats.DQ10rly
+const rpio = require('rpio')
 
 // access control allow headers
 app.use(function(req, res, next) {
@@ -49,6 +50,24 @@ app.get('/relay', function(req, res) {
   setTimeout(function () {
     relay.DQ.setChannel(id, false)
   }, time)
+})
+
+app.get('/lights', function(req, res) {
+  let pin = parseInt(req.query.pin)
+
+  rpio.init({mapping: 'gpio'})
+
+  rpio.open(pin, rpio.OUTPUT, rpio.LOW)
+
+  for (var i = 0; i < 5; i++) {
+        /* On for 1 second */
+        rpio.write(pin, rpio.HIGH);
+        rpio.sleep(1);
+
+        /* Off for half a second (500ms) */
+        rpio.write(pin, rpio.LOW);
+        rpio.msleep(500);
+  }
 })
 
 // set server
